cfc91d351028d5da9ad73a23ecd4a6add5c6e57e
manul-Pa2 <mihatitov305@gmail.com>
2025-12-11 01:48:45 +0200

    Fibonacci function
    Implement caching for Fibonacci sequence calculation

---
+++ Task_01_fibonacci.py
@@ -0,0 +1,16 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):         # базові випадки
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+
+        if n in cache:      # якщо вже обчислювали це n — повертаємо з кешу
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)    # рекурсія + повертаємо число
+        return cache[n]
+
+    return fibonacci     
